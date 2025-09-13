92f686eb82438e6a10abb3660473da81a34fdd29
Felipe Mattos <166661571+felipervm@users.noreply.github.com>
2025-09-13 16:25:36 +0000

    Updated HomeController, views, and layout for lab

--- LabMvcProject/Controllers/Home1Controller.cs
+++ LabMvcProject/Controllers/Home1Controller.cs
@@ -2,11 +2,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LabMvcProject.Controllers
 {
-    public class Home1Controller : Controller
+    public class HomeController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
+
